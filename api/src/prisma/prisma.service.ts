4232e30d4e79d134be662a107fe3b23cfa5de512
maxgreenhalghcc <maxgreenhalgh.work@gmail.com>
2025-10-27 22:44:15 +0000

    Update prisma.service.ts

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -3,8 +3,6 @@ import { PrismaClient, Prisma } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor(options?: Prisma.PrismaClientOptions) {
-    super(options);
   }
 
   async onModuleInit() {
