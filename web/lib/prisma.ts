b9214cd5fd4f612c6228e1dd81066257d9e254f7
maxgreenhalghcc <maxgreenhalgh.work@gmail.com>
2025-11-05 11:08:36 +0000

    different prisma import route

--- web/lib/prisma.ts
+++ web/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../api/node_modules/@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
